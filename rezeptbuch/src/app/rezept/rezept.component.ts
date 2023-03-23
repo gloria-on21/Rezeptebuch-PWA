124b3ec4e49168e17f52f7a1afac342a0f38d485
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2023-03-23 17:25:30 +0100

    updated recipe component

--- rezeptbuch/src/app/rezept/rezept.component.ts
+++ rezeptbuch/src/app/rezept/rezept.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-rezept',
   templateUrl: './rezept.component.html',
+  providers: [RecipeService]
 })
-export class RezeptComponent {
+export class RezeptComponent implements OnInit {
+
+
+  ngOnInit() {
+    
+  }
+
 
 }
