86f884d362ffd53f97e4a0c17e97b36ae2c99ecb
lau-byte <94553579+lau-byte@users.noreply.github.com>
2023-03-23 18:19:15 +0100

    Updated recipe-details.component.ts

--- rezeptbuch/src/app/rezept/recipe-details/recipe-details.component.ts
+++ rezeptbuch/src/app/rezept/recipe-details/recipe-details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ShoppingListService } from 'src/app/einkaufsliste/shopping-list.service';
+import { RecipeEditComponent } from '../recipe-edit/recipe-edit.component';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+selector: 'app-recipe-details',
+templateUrl: './recipe-details.component.html',
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit, OnDestroy{
+//Auch hier
+selectedRecipe!: Recipe;
+recipeId!: number;
+private subscription!: Subscription;
+
+ngOnInit() {
+this.subscription =this.activatedRoute.params.subscribe(
+params => {
+this.recipeId = +params['id'];
+this.selectedRecipe = this.recipeService.getRecipe(this.recipeId);}
+);
+}
+constructor(
+private recipeService: RecipeService,
+private sls: ShoppingListService,
+private activatedRoute: ActivatedRoute,
+private router: Router ){
 
 }
+
+//Mit dieser Funktion sollen die Zutaten von dem aktuellen Rezept auf eine Einkaufsliste gepusht werden
+
+onAddToList(){
+
+this.sls.addIngredients(this.selectedRecipe.ingridients);
+
+}
+
+onEdit(){
+
+this.router.navigate(['/rezepte',this.recipeId,'bearbeiten'])
+
+}
+onDelete(){
+
+this.recipeService.deleteRecipe(this.recipeId);
+this.router.navigate(['/rezepte'])
+}
+
+ngOnDestroy() {
+this.subscription.unsubscribe();
+
+}
+}
+
+// written by Laura
