2f178801fe3cb03e4b770bf450621f0c27f8c83b
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2023-03-23 17:23:37 +0100

    deleted unnecessary files

--- rezeptbuch/src/app/rezept/rezept.component.ts
+++ rezeptbuch/src/app/rezept/rezept.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rezept',
   templateUrl: './rezept.component.html',
-  styleUrls: ['./rezept.component.css']
 })
 export class RezeptComponent {
 
