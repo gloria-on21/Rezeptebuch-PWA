554aabc5e548d12ffdb92a5aab23b72aa97730f7
cbischoping <car.bischoping.21@lehre.mosbach.dhbw.de>
2023-03-23 17:11:00 +0100

    added einkaufsliste component with functions

--- rezeptbuch/src/app/app.module.ts
+++ rezeptbuch/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EinkaufslisteComponent } from './einkaufsliste/einkaufsliste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EinkaufslisteComponent
   ],
   imports: [
     BrowserModule,
---
+++ rezeptbuch/src/app/einkaufsliste/einkaufsliste.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+
+@Component({
+  selector: 'app-einkaufsliste',
+  templateUrl: './einkaufsliste.component.html',
+})
+export class EinkaufslisteComponent implements OnInit {
+  ingredients: Ingridient[] = [];
+  selectedIngredient!: Ingridient | null ;
+
+  constructor (private sls: ShoppingListService){
+  }
+
+  ngOnInit() {
+    this.ingredients = this.sls.getIngredients();
+
+  }
+
+  onSelectItem(ingredient:Ingridient){
+    this.selectedIngredient = ingredient;
+
+  }
+
+  onCleared(){
+    this.selectedIngredient = null;
+  }
+
+}
+//written by Carolin Bischoping 
