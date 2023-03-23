bbe4d0c879aa6428468070e27eb4d10180be4f36
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2023-03-23 17:43:36 +0100

    updated recipe start component

---
+++ rezeptbuch/src/app/rezept/recipe-start.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-start',
+  template: `<h1> Bitte ein Rezept eintragen</h1>`,
+})
+export class RecipeStartComponent {
+
+}
