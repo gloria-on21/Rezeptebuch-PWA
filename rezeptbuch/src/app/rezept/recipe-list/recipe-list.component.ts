0a9c73125c781194e3ac7a846b84183a86ab8daf
lau-byte <94553579+lau-byte@users.noreply.github.com>
2023-03-23 18:28:33 +0100

    Updated recipe-list.component.ts

--- rezeptbuch/src/app/rezept/recipe-list/recipe-list.component.ts
+++ rezeptbuch/src/app/rezept/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+selector: 'app-recipe-list',
+templateUrl: './recipe-list.component.html',
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+
+recipes: Recipe[] | undefined;
+
+constructor(private recipeService: RecipeService) {}
+//ngOnit ist für initialisierungsdaten gut, hier wollen wir unsere dummy werte der Rezepte als initialisierungsdaten haben. demeentsprechend verwenden wir hier unseren rezepte service
+ngOnInit(){
+
+this.recipes = this.recipeService.getRecipes();
+this.recipeService.recipesChanged.subscribe(
+(recipes: Recipe[])=> this.recipes = recipes
+);
 
 }
+
+}
+
+// written by Laura
