18c5e6de6c489fe2e6a5fdf1b9a9bcb692aaf65c
lau-byte <94553579+lau-byte@users.noreply.github.com>
2023-03-23 18:24:32 +0100

    Updated recipe-edit.component.ts

--- rezeptbuch/src/app/rezept/recipe-edit/recipe-edit.component.ts
+++ rezeptbuch/src/app/rezept/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Ingridient } from 'src/app/ingredient.model';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-added',
-  templateUrl: './recipe-added.component.html',
-  styleUrls: ['./recipe-added.component.css']
+selector: 'app-recipe-edit',
+templateUrl: './recipe-edit.component.html'
 })
-export class RecipeAddedComponent {
+export class RecipeEditComponent implements OnInit, OnDestroy{
+recipeForm!: FormGroup ;
+private recipeIndex!: number;
+private subscription!: Subscription;
+private isNew =true;
+private recipe!: Recipe | null;
+//Subscription um die Paramaeter zu verfolgen
+constructor(private recipeService: RecipeService,
+private router: Router,
+private route: ActivatedRoute){
 
 }
+ngOnDestroy(){
+this.subscription.unsubscribe();
+}
+
+ngOnInit() {
+
+this.subscription = this.route.params.subscribe(
+params =>{
+if(params.hasOwnProperty('id')){
+this.isNew = false;
+this.recipeIndex = +params['id'];
+this.recipe = this.recipeService.getRecipe(this.recipeIndex);
+}
+else{
+this.isNew = true;
+this.recipe = null;
+}
+}
+);
+
+let recipeName= null;
+let recipeImagePath = null;
+let recipeDescription = null;
+let recipeIngredients = new FormArray([]);
+//joa das laden funktioniert nicht ganz aber das funktioniert nicht wirklich weil ich keinen FormArray einfach so übergeben kann
+
+//Wenn wir nicht in den isnew Modus sind werden die Daten überschrieben
+if(!this.isNew){
+if(this.recipe?.hasOwnProperty('ingredients')){
+for (let ingredient of this.recipe.ingridients){
+recipeIngredients.push( new FormGroup({
+'name': new FormControl(ingredient.name, Validators.required),
+'amount': new FormControl(ingredient.amount, Validators.required)
+}));
+
+
+
+}
+
+}
+recipeName = this.recipe?.name;
+recipeImagePath = this.recipe?.imagePath
+recipeDescription = this.recipe?.description
+}
+
+
+//hier werden die ganzen Controlls übergeben:
+//hier erstellen wir über die von Angular verfügbare Klasse eine Form
+
+this.recipeForm = new FormGroup({
+'name': new FormControl(recipeName, Validators.required),
+'imagePath': new FormControl(recipeImagePath, Validators.required),
+'description': new FormControl(recipeDescription, Validators.required),
+'ingredients': recipeIngredients
+
+});
+
+}
+
+onSubmit(){
+const newRecipe = this.recipeForm.value;
+if(this.isNew){
+this.recipeService.addRecipe(newRecipe)
+}
+else {
+this.recipeService.editRecipe(this.recipe, newRecipe)
+}
+this.onNavigateBack();
+}
+
+onCancel(){
+this.onNavigateBack();
+
+}
+onNavigateBack(){
+this.router.navigate(['/'])
+
+
+}
+onAddIngredientControl(name: string, amount: string){
+(<FormArray>this.recipeForm.get('ingredients')).push(
+new FormGroup({
+'name': new FormControl(name, Validators.required),
+'amount': new FormControl(amount, Validators.required),
+
+})
+
+);
+
+}
+onRemoveIngredientControl(index: number){
+(<FormArray>this.recipeForm.get('ingredients')).removeAt(index)
+
+}
+}
+
+// written by Laura
